20d4211936fc62a7b6e46520bb4f18546bfb090b
yumenome <waiyan2015.wy65@gmail.com>
2023-11-26 22:04:24 +0630

    s

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = () => {
 return (
 <Stack direction='row' justifyContent='space-around' 
   sx={{ gap: { sm: '290px', xs: '10px'}, mt: '10px', justifyContent: 'none', px: '20px', alignItems: 'center'}} >
-    <Link to="/GiM">
+    <Link to="/">
       <img src={logo} style={{ width: '60px', height: '60px', marginRight: '30px', marginLeft: '50px'}} />
     </Link>
     <Stack direction='row' gap='50px' alignItems='flex-end'>
-      <Link to="/GiM" style={{ textDecoration: 'none', color: '#3A1212'  }}>
+      <Link to="/" style={{ textDecoration: 'none', color: '#3A1212'  }}>
         HOME
       </Link>
       <a href='#exercises' style={{ textDecoration: 'none', color: '#3A1212'}}>
